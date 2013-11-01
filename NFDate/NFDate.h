92bd2a0af987b32073f1fb69c51a9085d38f9bd1
Andrew Williams <awilliams@nextfaze.com>
2013-11-01 11:45:48 +1030

    Initial Commit

---
+++ NFDate/NFDate.h
@@ -0,0 +1,13 @@
+//
+//  NFDate.h
+//  NFDate
+//
+//  Created by Andrew Williams on 1/11/2013.
+//  Copyright (c) 2013 NextFaze. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NFDate : NSObject
+
+@end
---
+++ NFDate/NFDate.m
@@ -0,0 +1,13 @@
+//
+//  NFDate.m
+//  NFDate
+//
+//  Created by Andrew Williams on 1/11/2013.
+//  Copyright (c) 2013 NextFaze. All rights reserved.
+//
+
+#import "NFDate.h"
+
+@implementation NFDate
+
+@end
